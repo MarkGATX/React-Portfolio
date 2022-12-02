a24d3a203314ce33ebb620ee8c8adac5f73df032
Mark Gardner <themarkgardner@gmail.com>
2022-12-01 19:41:00 -0600

    added devicons css to index and adjusted some css

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,35 +3,35 @@
 export default function AboutMe() {
 
     return (
-        <section class="aboutMe col-md-3 col-lg-3 ">
-            <h2 class="col-12 mb-4">About me</h2>
+        <section className="aboutMe col-md-3 col-lg-3 ">
+            <h2 className="col-12 mb-4">About me</h2>
             <p>I'm a creative full-stack developer and multi-hyphenate who loves learning and creating new
                 things. </p>
             <p>The experience a person has using any media should be easy, enjoyable, and sometimes even
                 surprising. </p>
-            <p class="mb-5">Thanks for stopping by and please reach out to say hello.</p>
+            <p className="mb-5">Thanks for stopping by and please reach out to say hello.</p>
             <h5 id="question"></h5>
-            <p id="answer" class="mb-5"></p>
+            <p id="answer" className="mb-5"></p>
             <h5 id="techSkills" >Tech skills:</h5>
-            <div>
-                <i title="Sequelize" class="devicon-sequelize-plain"></i>
-                <i title="Bootstrap" class="devicon-bootstrap-plain"></i>
-                <i title="Handlebars" class="devicon-handlebars-plain"></i>
-                <i title="Heroku" class="devicon-heroku-plain"></i>
-                <i title="jquery" class="devicon-jquery-plain"></i>
-                <i title="MySQL" class="devicon-mysql-plain"></i>
-                <i title="MongoDB" class="devicon-mongodb-plain"></i>
-                <i title="vscode" class="devicon-vscode-plain"></i>
-                <i title="Javascript" class="devicon-javascript-plain"></i>
-                <i title="CSS3" class="devicon-css3-plain"></i>
-                <i title="NodeJS" class="devicon-nodejs-plain"></i>
-                <i title="Wordpress" class="devicon-wordpress-plain"></i>
-                <i title="Photoshop" class="devicon-photoshop-plain"></i>
-                <i title="Premiere Pro" class="devicon-premierepro-plain"></i>
-                <i title="After Effects" class="devicon-aftereffects-plain"></i>
-                <i title="Blender" class="devicon-blender-original"></i>
-                <img title="Greensock" class="icon " src="./public/images/gsap-greensock.svg" />
-                    <img title="Adobe Animate" class="icon" src="./public/images/Adobe_Animate_CC_icon_(2020).svg" />
+            <div className="d-flex flex-wrap">
+                <i title="Sequelize" className="devicon-sequelize-plain"></i>
+                <i title="Bootstrap" className="devicon-bootstrap-plain"></i>
+                <i title="Handlebars" className="devicon-handlebars-plain"></i>
+                <i title="Heroku" className="devicon-heroku-plain"></i>
+                <i title="jquery" className="devicon-jquery-plain"></i>
+                <i title="MySQL" className="devicon-mysql-plain"></i>
+                <i title="MongoDB" className="devicon-mongodb-plain"></i>
+                <i title="vscode" className="devicon-vscode-plain"></i>
+                <i title="Javascript" className="devicon-javascript-plain"></i>
+                <i title="CSS3" className="devicon-css3-plain"></i>
+                <i title="NodeJS" className="devicon-nodejs-plain"></i>
+                <i title="Wordpress" className="devicon-wordpress-plain"></i>
+                <i title="Photoshop" className="devicon-photoshop-plain"></i>
+                <i title="Premiere Pro" className="devicon-premierepro-plain"></i>
+                <i title="After Effects" className="devicon-aftereffects-plain"></i>
+                <i title="Blender" className="devicon-blender-original"></i>
+                <img title="Greensock" className="icon " src="./src/images/gsap-greensock.svg" />
+                    <img title="Adobe Animate" className="icon" src="./public/images/Adobe_Animate_CC_icon_(2020).svg" />
                     </div>
 
                 </section>
