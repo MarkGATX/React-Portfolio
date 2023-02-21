7357e9e71ff31f503c7d131ee68aa574410990d2
Mark Gardner <themarkgardner@gmail.com>
2023-02-21 12:30:55 -0600

    fixed error onload where browser didnt find init for animation. Simply moved the setTimeout to body onLoad

--- src/components/LinkModal/index.js
+++ src/components/LinkModal/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Modal from 'react-bootstrap/Modal';
-// import liveIcon from "../images/live_path_icon.svg";
 import liveIcon from "../../images/live_path_icon.svg";
 import "./LinkModal.css";
 
 
-
 export default function LinkModal({ livePath, gitPath, setShow, vidlink, show, title, desc, role, imgs }) {
     const imgArray = imgs.map((image) => image.map((imageData, key) => <img key={key} src={imageData} alt={title} />))
-    if (vidlink === undefined) {
+
+     if (vidlink === undefined) {
         return (
             <>
                 <Modal show={show} onHide={() => setShow(false)} size="lg">
@@ -26,7 +25,7 @@ export default function LinkModal({ livePath, gitPath, setShow, vidlink, show, t
                         <div className="d-flex flex-wrap justify-content-around detailsLinks" >
                             <a href={livePath} target="_blank" rel="noreferrer" className="livePath col-5 text-decoration-none">
                                 <div className="mb-2 d-flex justify-content-start">
-                                    <img className='icon ' src={liveIcon} alt="icon for live site" title="Icon for live site"  />Live site
+                                    <img className='icon ' src={liveIcon} alt="icon for live site" title="Icon for live site" />Live site
                                 </div>
                             </a>
                             <a href={gitPath} target="_blank" rel="noreferrer" className="gitPath col-5 text-decoration-none">
